89c668b02bd6fa4fd47eb64d0cf25aaf621570bd
jovanniblanco14 <159047765+jovanniblanco14@users.noreply.github.com>
2024-02-16 10:06:30 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,1 +1,33 @@
-# CIS 129 lab for pulling input from users
+#Starting with constant
+TAX = .06
+COFFEE = 5
+MUFFINS = 4
+
+#I will start with welcome message and a prompt for the user
+print('***************************************')
+print('My Coffee and Muffin Shop')
+print('Number of coffees bought?')
+coffeesPurchased = int(input())
+print('Number of muffins bought?')
+muffinsPurchased = int(input())
+print('***************************************')
+#
+#
+print('***************************************')
+#I had difficulty figuring out how to properly set the variables
+#through trial and error I eventually figured it out
+coffeeAmount = coffeesPurchased * COFFEE
+muffinAmount = muffinsPurchased * MUFFINS
+orderTotal = coffeeAmount + muffinAmount
+afterTax = orderTotal * TAX
+totalAfterTax = orderTotal + afterTax
+
+#The main issue I had at this point was mixing up coffeeAmount and
+#coffeesPurchased. In hindsight it is more obvious
+print(str(coffeesPurchased) + ' Coffee at $5 each: $ ' + str(coffeeAmount))
+print(str(muffinsPurchased) + ' Muffins at $4 each: $ ' + str(muffinAmount))
+print('6% Tax: $' + str(afterTax))
+print('---------')
+print('Total: $ ' + str(totalAfterTax))
+print('***************************************')
+
